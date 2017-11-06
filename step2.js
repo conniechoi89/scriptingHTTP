1ab0c6565e4abdea0aeef9f0138c73db892cd6fd
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-06 20:41:43 +0000

    step 2 boom

---
+++ step2.js
@@ -0,0 +1,18 @@
+function getAndPrintHTML () {
+  var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+    console.log(response);
+
+  let responseBodyString = '';
+    response.on('data', function (chunk) {
+      responseBodyString += chunk;
+      console.log(chunk, '\n');
+    });
+  });
+}
+getAndPrintHTML();
