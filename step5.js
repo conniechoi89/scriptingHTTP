66820f9c0d8c962f4def086f2cde3a6cbf304cf0
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-06 22:22:18 +0000

    step 5 boom

--- step5.js
+++ step5.js
@@ -1,11 +1,13 @@
-module.exports = function getHTML (options, callback) {
-
-
-};
-
 var getHTML = require('./http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step5.html'
 };
+
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
