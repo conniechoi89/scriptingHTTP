cc30a3c24f0e4d5f4dea5e51ea68013a65994a4c
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-06 21:57:02 +0000

    step4 boom

--- step4.js
+++ step4.js
@@ -1,13 +1,42 @@
+var https = require('https');
+
 function getHTML (options, callback) {
+  https.get(options, function(response) {
 
-
+    let responseBodyString = '';
+    response.on('data', function (chunk) {
+      responseBodyString += chunk;
+    callback(responseBodyString);
+  });
+});
 }
-
 function printHTML (html) {
   console.log(html);
 }
+
+// function upperCasePrintHTML (html) {
+//   console.log(html.toUpperCase())
+// }
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
 };
+
+// getAndPrintHTML(requestOptions);
+getHTML(requestOptions, printHTML);
+
+// getHTML(requestOptions, upperCasePrintHTML);
+
+
+
+// function getAndPrintHTML (options) {
+
+
+//   https.get(options, function(response) {
+//     response.setEncoding('utf8');
+//     console.log(response);
+
+
+//     });
+// }
