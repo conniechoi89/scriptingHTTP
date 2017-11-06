b7eb89f813586fbfaea34d86f06df5993086191b
Connie (Moon Hyung) Choi <connie_choi89@hotmail.com>
2017-11-06 21:06:28 +0000

    step3 boom

--- step3.js
+++ step3.js
@@ -1,9 +1,18 @@
 function getAndPrintHTML (options) {
+  var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+    console.log(response);
 
-
+  let responseBodyString = '';
+    response.on('data', function (chunk) {
+      responseBodyString += chunk;
+      console.log(chunk, '\n');
+    });
+  });
 }
-
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/steps.html'
-};
+getAndPrintHTML();
